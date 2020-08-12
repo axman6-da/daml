e2b2db394e35c9301a5203a2f3daf7f0da1f9338
Remy Haemmerle <Remy.Haemmerle@daml.com>
2020-08-12 13:08:59 +0200

    fix comparison according the spec

--- daml-lf/interpreter/src/main/scala/com/digitalasset/daml/lf/speedy/svalue/Equality.scala
+++ daml-lf/interpreter/src/main/scala/com/digitalasset/daml/lf/speedy/svalue/Equality.scala
@@ -3,90 +3,99 @@
 
 package com.daml.lf.speedy
 package svalue
+import com.daml.lf.speedy.SError.SErrorCrash
 
 import scala.collection.JavaConverters._
 
 private[lf] object Equality {
 
   // Equality between two SValues of same type.
-  // Note it is not reflexive, in other words there is some value `v`
-  // such that `areEqual(v, v)` returns `False`).
   // This follows the equality defined in the daml-lf spec.
+  @throws[SErrorCrash]
   def areEqual(x: SValue, y: SValue): Boolean = {
     import SValue._
 
     var success = true
-    val stack = new Stack
-    stack.push(x, y)
+    var xs = List(Iterator(x))
+    var ys = List(Iterator(y))
 
-    while (success && stack.nonEmpty) success = stack.pop() match {
-      case (x: SPrimLit, y: SPrimLit) =>
-        x == y
-      case (SEnum(_, _, rank1), SEnum(_, _, rank2)) =>
-        rank1 == rank2
-      case (SRecord(_, _, args1), SRecord(_, _, args2)) =>
-        stack.push(args1.iterator().asScala, args2.iterator().asScala)
-        true
-      case (SVariant(_, _, rank1, arg1), SVariant(_, _, rank2, arg2)) =>
-        stack.push(arg1, arg2)
-        rank1 == rank2
-      case (SList(lst1), SList(lst2)) =>
-        stack.push(lst1.iterator, lst2.iterator)
-        lst1.length == lst2.length
-      case (SOptional(xs), SOptional(ys)) =>
-        stack.push(xs.iterator, ys.iterator)
-        xs.nonEmpty == ys.nonEmpty
-      case (STextMap(map1), STextMap(map2)) =>
-        val keys = map1.keys
-        stack.push(keys.iterator.map(map1), keys.iterator.map(map2))
-        map1.size == map2.size && keys.forall(map2.keySet.contains)
-      case (SGenMap(map1), SGenMap(map2)) =>
-        stack.push(map1.values.iterator, map2.values.iterator)
-        stack.push(map1.keys.iterator, map2.keys.iterator)
-        map1.size == map2.size
-      case (SStruct(fields1, args1), SStruct(fields2, args2)) =>
-        stack.push(args1.iterator().asScala, args2.iterator().asScala)
-        fields1 sameElements fields2
-      case (SAny(t1, v1), SAny(t2, v2)) =>
-        stack.push(v1, v2)
-        t1 == t2
-      case (STypeRep(t1), STypeRep(t2)) =>
-        t1 == t2
-      case _ =>
-        false
+    while (success && xs.nonEmpty) {
+      // invariant: xs.length == ys.length
+
+      if (xs.head.hasNext) {
+        if (ys.head.hasNext) {
+          (xs.head.next(), ys.head.next()) match {
+            case (x: SPrimLit, y: SPrimLit) =>
+              success = x == y
+            case (SEnum(_, _, rank1), SEnum(_, _, rank2)) =>
+              success = rank1 == rank2
+            case (SRecord(_, _, args1), SRecord(_, _, args2)) =>
+              xs = args1.iterator().asScala :: xs
+              ys = args2.iterator().asScala :: ys
+            case (SVariant(_, _, rank1, arg1), SVariant(_, _, rank2, arg2)) =>
+              xs = Iterator(arg1) :: xs
+              ys = Iterator(arg2) :: ys
+              success = rank1 == rank2
+            case (SList(lst1), SList(lst2)) =>
+              xs = lst1.iterator :: xs
+              ys = lst2.iterator :: ys
+            case (SOptional(x1), SOptional(x2)) =>
+              xs = x1.iterator :: xs
+              ys = x2.iterator :: ys
+            case (STextMap(map1), STextMap(map2)) =>
+              val keys1 = map1.keys.toSeq.sorted
+              val keys2 = map2.keys.toSeq.sorted
+              xs = new Interlace(keys1.iterator.map(SText), keys1.iterator.map(map1)) :: xs
+              ys = new Interlace(keys2.iterator.map(SText), keys2.iterator.map(map2)) :: ys
+            case (SGenMap(map1), SGenMap(map2)) =>
+              xs = new Interlace(map1.keys.iterator, map1.values.iterator) :: xs
+              ys = new Interlace(map2.keys.iterator, map2.values.iterator) :: ys
+            case (SStruct(_, args1), SStruct(_, args2)) =>
+              xs = args1.iterator().asScala :: xs
+              ys = args2.iterator().asScala :: ys
+            case (SAny(t1, v1), SAny(t2, v2)) =>
+              xs = Iterator(v1) :: xs
+              ys = Iterator(v2) :: ys
+              success = t1 == t2
+            case (STypeRep(t1), STypeRep(t2)) =>
+              success = t1 == t2
+            case _ =>
+              throw SErrorCrash("try to compare incomparable type")
+          }
+        } else {
+          success = false
+        }
+      } else {
+        if (ys.head.hasNext) {
+          success = false
+        } else {
+          xs = xs.tail
+          ys = ys.tail
+        }
+      }
     }
+
     success
+
   }
 
-  private[this] class Stack {
-    // Invariants:
-    // - xs.length == ys.length
-    // - xs.indices.forall(i => xs(i).size == ys(i).size)
-    // - xs.forall(_.nonEmpty)
-    private[this] var xs = List.empty[Iterator[SValue]]
-    private[this] var ys = List.empty[Iterator[SValue]]
+  // Assumes iterLeft.size == iterRight.size
+  private[this] class Interlace[X](iterLeft: Iterator[X], iterRight: Iterator[X])
+      extends Iterator[X] {
+    private[this] var left = true
 
-    def nonEmpty: Boolean = xs.nonEmpty
+    override def hasNext: Boolean =
+      if (left) iterLeft.hasNext else iterRight.hasNext
 
-    def pop(): (SValue, SValue) = {
-      val tuple = (xs.head.next(), ys.head.next())
-      if (xs.head.isEmpty) {
-        xs = xs.tail
-        ys = ys.tail
+    override def next(): X = {
+      if (left) {
+        left = false
+        iterLeft.next()
+      } else {
+        left = true
+        iterRight.next()
       }
-      tuple
     }
-
-    def push(x: SValue, y: SValue): Unit =
-      push(Iterator(x), Iterator(y))
-
-    // Assume xs and ys have the same size.
-    def push(xs: Iterator[SValue], ys: Iterator[SValue]): Unit =
-      if (xs.nonEmpty) {
-        this.xs = xs :: this.xs
-        this.ys = ys :: this.ys
-      }
-
   }
 
 }
